ebc023e172e8e3af3cb7ab12d4138f93864c3751
algui <alguidrag@gmail.com>
2020-03-26 15:03:04 -0400

    Alteração de Pasta

---
+++ frontendg/src/App.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+import './global.css';
+
+import Routes from './routes'; 
+
+
+//JSX (JavaScript XML)
+
+
+function App() {
+ 
+
+
+  return (
+    <Routes/>
+    );
+}
+
+export default App;
